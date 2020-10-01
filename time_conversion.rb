3576a36fae0fd299562244f6c07130eace0cac52
kseggelke918 <katie.seggelke@gmail.com>
2020-09-30 20:26:03 -0600

    add time_conversion

---
+++ time_conversion.rb
@@ -0,0 +1,64 @@
+# Given a time in -hour AM/PM format, convert it to military (24-hour) time.
+
+# Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
+# - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
+
+# Example
+
+
+# Return '12:01:00'.
+
+
+# Return '00:01:00'.
+
+# Function Description
+
+# Complete the timeConversion function in the editor below. It should return a new string representing the input time in 24 hour format.
+
+# timeConversion has the following parameter(s):
+
+# string s: a time in  hour format
+# Returns
+
+# string: the time in  hour format
+# Input Format
+
+# A single string  that represents a time in -hour clock format (i.e.:  or ).
+
+# Constraints
+
+# All input times are valid
+# Sample Input 0
+
+# 07:05:45PM
+# Sample Output 0
+
+# 19:05:45
+
+#!/bin/ruby
+
+#
+# Complete the timeConversion function below.
+#
+def timeConversion(s)
+    if s.include? "AM"
+      return s
+    else 
+      time_array = s.split(":")
+      time_array[0] = time_array[0].to_i + 12
+      time_array[2] = time_array[2].delete "PM"
+      return time_array.join(":")  
+    end 
+  end
+  
+  fp = File.open(ENV['OUTPUT_PATH'], 'w')
+  
+  s = gets.to_s.rstrip
+  
+  result = timeConversion s
+  
+  fp.write result
+  fp.write "\n"
+  
+  fp.close()
+  
