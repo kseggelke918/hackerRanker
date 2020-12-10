5a5645de5f123aaa90bc214e568f75f637393d64
kseggelke918 <katie.seggelke@gmail.com>
2020-12-10 10:45:18 -0700

    add range.rb

---
+++ Ruby/range.rb
@@ -0,0 +1,7 @@
+# Sometimes, we have to check whether a given number a is within the range b and c (where b  c, and both inclusive ).
+
+# checks whether a is within the range of b and c by calling the method range 
+# (which we have defined for you as a method for this example) on a and 
+# passing b and c as arguments.
+
+a.range?(b, c)
