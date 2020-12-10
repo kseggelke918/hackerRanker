ae4d09f4681b6a1dcf480af8b3584aa01d5d5389
kseggelke918 <katie.seggelke@gmail.com>
2020-12-10 11:01:59 -0700

    add methods to practice removing items from arrays

--- Ruby/remove_elements_from_array.rb
+++ Ruby/remove_elements_from_array.rb
@@ -19,3 +19,24 @@ arr.delete_at(2)
 arr.delete(5)
 # => 5 (returns item being deleted)
 # updated array: [6, 3, 1, 2, 4, 3, 3] (no more 5s)
+
+
+def end_arr_delete(arr)
+    # delete the element from the end of the array and return the deleted element
+    arr.pop
+end
+
+def start_arr_delete(arr)
+    # delete the element at the beginning of the array and return the deleted element
+    arr.shift
+end
+
+def delete_at_arr(arr, index)
+    # delete the element at the position #index
+    arr.delete_at(index)
+end
+
+def delete_all(arr, val)
+    # delete all the elements of the array where element = val
+    arr.delete(val)
+end
