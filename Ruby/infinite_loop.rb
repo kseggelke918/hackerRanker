bebe0ac90eef62d1b1ebbcf1e6654cce8123c058
kseggelke918 <katie.seggelke@gmail.com>
2021-01-12 14:00:59 -0700

    add infinite loop file and code

---
+++ Ruby/infinite_loop.rb
@@ -0,0 +1,20 @@
+# infinite loop 
+loop do 
+
+end 
+
+# break conditions
+
+if condition 
+    break 
+end 
+
+# OR 
+
+break if condition
+
+# example: 
+loop do 
+    coder.practice 
+    break if coder.oh_one? == true 
+end 
