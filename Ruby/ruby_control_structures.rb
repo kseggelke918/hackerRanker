1afb2a6ef8257683da0845c194b9b20b4f7e17ba
kseggelke918 <katie.seggelke@gmail.com>
2021-01-12 14:08:56 -0700

    add case statement in ruby_control_structures.rb

--- Ruby/ruby_control_structures.rb
+++ Ruby/ruby_control_structures.rb
@@ -15,3 +15,20 @@
 # if TestCase, output "It's a TestCase!"
 # if Contest, output "It's a Contest!"
 # for any other object, output "It's an unknown model"
+
+def identify_class(obj)
+    # write your case control structure here
+    case obj 
+        when Hacker 
+            puts "It's a Hacker!"
+        when Submission 
+            puts "It's a Submission!"
+        when TestCase
+            puts "It's a TestCase!"
+        when Contest 
+            puts "It's a Contest!"
+        else 
+            puts "It's an unknown model"
+    end 
+        
+end
