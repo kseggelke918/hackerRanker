56b2c842c8c25193e50c5ea23886e3e05c17bf15
kseggelke918 <katie.seggelke@gmail.com>
2021-01-12 14:05:39 -0700

    add ntil.rb and example code

---
+++ Ruby/until.rb
@@ -0,0 +1,19 @@
+# call the method coder.practice until coder.oh_one? becomes true 
+
+coder.practice until coder.oh_one? 
+
+# option 1
+
+while not <condition>
+    <code>
+end 
+
+# option 2
+
+until <condition> 
+    <code>
+end 
+
+# option 3 
+
+<code> until <codition>
